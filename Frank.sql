5262d8b6131a551fd4eff9318a334b72a68b0e31
RaulAguilarZ <raguilarz79@gmail.com>
2024-09-17 19:34:06 -0500

    Add files via upload

---
+++ Frank.sql
@@ -0,0 +1,3 @@
+SELECT SUM(imp_deposito) FROM sic_movtos_coloca where fec_valor between '20230101'and'20231231'
+
+SELECT TOP 2 *FROM sic_movtos_coloca where fec_valor between '20230101'and'20231231'
